13e05ffcd2fa93c9ecf880607994ee985a5906fa
brahma <brahma@gopassport.com>
2016-11-25 23:26:12 +0530

    add ImagePickerController

--- MemeMe-v1.0/ViewController.swift
+++ MemeMe-v1.0/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet var memeImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pickAnImage(_ sender: AnyObject) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let selectedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            memeImage.image = selectedImage
+        } else {
+            print("Something went wrong in selecting image")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
 
 }
 
